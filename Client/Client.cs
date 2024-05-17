f5aad90a8c0c4c4eab2e369cd28e319b24fefc74
Gleb <gleb.0997@gmail.com>
2024-05-17 15:03:40 +0300

    Initial commit

---
+++ Client/Client.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Diagnostics;
+using System.Net.Sockets;
+using System.Text;
+
+class Client()
+{
+    const string ServerAddress = "127.0.0.1";
+    const int Port = 8888;
+    const int ArraySize = 20000;
+    const int TestRuns = 100;
+
+    static void Main(string[] args)
+    {
+        double totalTime = 0;
+        double avgTime = 0;
+        Random rnd = new Random();
+
+        for (int run = 0; run < TestRuns; run++)
+        {
+            int[] array = new int[ArraySize];
+            for (int i = 0; i < ArraySize; i++)
+            {
+                array[i] = rnd.Next();
+            }
+
+            TcpClient client = new TcpClient(ServerAddress, Port);
+            NetworkStream stream = client.GetStream();
+
+            try
+            {
+                Stopwatch stopwatch = Stopwatch.StartNew();
+
+                byte[] lengthBuffer = BitConverter.GetBytes(array.Length);
+                stream.Write(lengthBuffer, 0, lengthBuffer.Length);
+
+                byte[] buffer = new byte[array.Length * sizeof(int)];
+                Buffer.BlockCopy(array, 0, buffer, 0, buffer.Length);
+                stream.Write(buffer, 0, buffer.Length);
+
+                byte[] receivedBuffer = new byte[array.Length * sizeof(int)];
+                stream.Read(receivedBuffer, 0, receivedBuffer.Length);
+
+                stopwatch.Stop();
+                totalTime += stopwatch.Elapsed.TotalMicroseconds;
+            }
+            finally
+            {
+                client.Close();
+            }
+        }
+
+        avgTime = totalTime / TestRuns;
+
+        Console.WriteLine($"Average time: {avgTime} ms.");
+    }
+}
---
+++ Lab4/Server.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+class Server
+{
+    const int Port = 8888;
+
+    static void Main(string[] args)
+    {
+        TcpListener server = null;
+        try
+        {
+            server = new TcpListener(IPAddress.Any, Port);
+            server.Start();
+
+            Console.WriteLine("Server started...");
+
+            while (true)
+            {
+                TcpClient client = server.AcceptTcpClient();
+                Task.Run(() => HandleClient(client));
+            }
+        }
+        finally
+        {
+            server?.Stop();
+        }
+    }
+
+    private static void HandleClient(TcpClient client)
+    {
+        NetworkStream stream = client.GetStream();
+
+        try
+        {
+            byte[] lenghtBuffer = new byte[4];
+            stream.Read(lenghtBuffer, 0, lenghtBuffer.Length);
+            int length = BitConverter.ToInt32(lenghtBuffer, 0);
+
+            byte[] buffer = new byte[length * sizeof(int)];
+            stream.Read(buffer, 0, buffer.Length);
+
+            int[] array = new int[length];
+            Buffer.BlockCopy(buffer, 0, array, 0, buffer.Length);
+
+            QuickSort(array, 0, array.Length - 1);
+
+            Buffer.BlockCopy(array, 0, buffer, 0, buffer.Length);
+            stream.Write(buffer, 0, buffer.Length);
+        }
+        finally
+        {
+            client.Close();
+        }
+    }
+
+    private static void QuickSort(int[] array, int low, int high)
+    {
+        if (low < high)
+        {
+            int pivotIndex = Partition(array, low, high);
+            QuickSort(array, low, pivotIndex - 1);
+            QuickSort(array, pivotIndex + 1, high);
+        }
+    }
+
+    private static int Partition(int[] array, int low, int high)
+    {
+        int pivot = array[high];
+        int i = low - 1;
+
+        for (int j = low; j < high; j++)
+        {
+            if (array[j] <= pivot)
+            {
+                i++;
+                Swap(array, i, j);
+            }
+        }
+
+        Swap(array, i + 1, high);
+        return i + 1;
+    }
+
+    private static void Swap(int[] array, int i, int j)
+    {
+        int temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+}
